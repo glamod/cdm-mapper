d6813d02f552229a4ac66f7b7830afb14c633861
perezgonzalez-irene <iregon@noc.ac.uk>
2019-12-02 08:31:37 +0000

    Added table reader

--- __init__.py
+++ __init__.py
@@ -2,4 +2,5 @@
 from .mapper.mapper import map_model as map_model
 from .table_writer.table_writer import cdm_to_ascii as cdm_to_ascii
 from .table_writer.table_writer import table_to_ascii as table_to_ascii
+from .table_reader.table_reader import read_tables as read_tables
 from .gridded_stats import gridded_stats

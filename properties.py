e026906e20ba81fb2bac16f32876959d3dfcf6e8
Irene Perez Gonzalez <iregon@jasmin-sci1.ceda.ac.uk>
2020-03-02 09:24:23 +0000

    Added support to pandas nullable integers

--- properties.py
+++ properties.py
@@ -12,8 +12,14 @@ cdm_tables = ['header','observations-at','observations-sst',
 # DATA TYPES ------------------------------------------------------------------
 numpy_integers = ['int8','int16','int32','int64','uint8','uint16','uint32','uint64']
 numpy_floats = ['float16','float32','float64']
+
+pandas_nan_integers = {'int8':'Int8','int16':'Int16','int32':'Int32',
+                       'int64':'Int64','uint8':'UInt8','uint16':'UInt16',
+                       'uint32':'UInt32','uint64':'UInt64'}
+
 numeric_types = numpy_integers.copy()
 numeric_types.extend(numpy_floats)
+numeric_types.extend(pandas_nan_integers.values())
 object_types = ['str','object','key','datetime']
 
 data_types = object_types.copy()
